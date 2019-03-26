d8fbe6a31ebcc0fe27e81e595baf73c114ffa7b1
akim0409 <autumnkim0409@gmail.com>
2019-03-26 17:14:15 -0400

    Prepare for initial Heroku push

---
+++ frontend/components/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Provider } from "react-redux";
+
+const App = () => {
+  <div>
+    <header>
+      <h1>WELCOME TO AUTUBE</h1>
+    </header>
+  </div>;
+};
+
+export default App;
