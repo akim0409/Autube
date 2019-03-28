1a6297ec023aae70e3541f78ce9f13af9b4a8c68
akim0409 <autumnkim0409@gmail.com>
2019-03-27 21:14:03 -0400

    create route_utils

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Home from "../home/home";
+// import NavBarContainer from "./nav_bar/nav_bar_container";
 import { Route } from "react-router-dom";
-// import NavBarContainer from './nav_bar/nav_bar_container';
+import { AuthRoute, ProtectedRoute } from "../util/route_utils";
 
 import SignUpContainer from "./session/signup_container";
 
 const App = () => (
   <div>
-    {/* <Route path='/' component={NavBarContainer} /> */}
+    {/* <Route path="/" component={NavBarContainer} /> */}
     <Route exact path="/" component={Home} />
-    <Route path="/signup" component={SignUpContainer} />
+    {/* <ProtectedRoute path="something like video upload page" component={VideoUploadContainer}/> */}
+    <AuthRoute path="/signup" component={SignUpContainer} />
   </div>
 );
 
