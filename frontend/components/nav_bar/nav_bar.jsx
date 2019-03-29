ddce8af637f10342fd1cdca751482976b7bcea22
akim0409 <autumnkim0409@gmail.com>
2019-03-29 12:08:41 -0400

    back to normal

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -8,38 +8,11 @@ export default ({ currentUser, logout }) => {
       <button onClick={logout}>Logout</button>
     </div>
   ) : (
-    <nav className="navbar">
-      <div className="nav-left">
-        <div className="nav-logo">
-          <img src="img/autube-logo.svg" alt="" />
-        </div>
-      </div>
-
-      <div className="search-container">
-        <form className="search-form">
-          <div className="search-box">
-            <input type="text" placeholder="Search" />
-          </div>
-          <button type="submit" className="search-button">
-            <i className="fa fa-search" />
-          </button>
-        </form>
-      </div>
-
-      <div className="nav-right">
-        <a href="" className="nav-dropdown">
-          <i className="fas fa-video" />
-        </a>
-        <Link to="/login" className="signin-link">
-          Sign in
-        </Link>
-      </div>
-    </nav>
-    // <div>
-    //   <Link to="/signup">Sign Up</Link>
-    //   &nbsp;/&nbsp;
-    //   <Link to="/login">Sign In</Link>
-    // </div>
+    <div>
+      <Link to="/signup">Sign Up</Link>
+      &nbsp;/&nbsp;
+      <Link to="/login">Sign In</Link>
+    </div>
   );
   return (
     <header>
